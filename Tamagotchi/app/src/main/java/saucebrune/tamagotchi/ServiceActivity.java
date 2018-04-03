68f4942ae657d951efe1f97f6f50ab00cb8f79fb
CEGEPJONQUIERE\TreAl1133111 <treal1133111@etu.cegepjonquiere.ca>
2018-04-03 15:39:46 -0400

    Commencement du développement du service

---
+++ Tamagotchi/app/src/main/java/saucebrune/tamagotchi/ServiceActivity.java
@@ -0,0 +1,22 @@
+package saucebrune.tamagotchi;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by TreAl1133111 on 2018-04-03.
+ */
+
+public class ServiceActivity extends IntentService {
+    public ServiceActivity() {
+        super("ServiceActivity");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+
+
+}
